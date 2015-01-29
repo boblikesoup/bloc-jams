3f1b4c444547b61b54d4cfcd1ad01879da509649
boblikesoup <johnuke@gmail.com>
2015-01-28 16:14:56 -0800

    Built a SongPlayer service to share song dota accross views.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,19 @@
-
 // Example album.
 var albumPicasso = {
- name: 'The Colors',
- artist: 'Pablo Picasso',
- label: 'Cubism',
- year: '1881',
- albumArtUrl: '/images/album-placeholder.png',
+	name: 'The Colors',
+	artist: 'Pablo Picasso',
+	label: 'Cubism',
+	year: '1881',
+	albumArtUrl: '/images/album-placeholder.png',
 
- songs: [
- { name: 'Blue', length: '4:26' },
- { name: 'Green', length: '3:14' },
- { name: 'Red', length: '5:01' },
- { name: 'Pink', length: '3:21'},
- { name: 'Magenta', length: '2:15'}
- ]
+	songs: [
+	{ name: 'Blue', length: '4:26' },
+	{ name: 'Green', length: '3:14' },
+	{ name: 'Red', length: '5:01' },
+	{ name: 'Pink', length: '3:21'},
+	{ name: 'Magenta', length: '2:15'}
+	]
 };
-
 
 blocJams = angular.module('BlocJams', ['ui.router']);
 
@@ -25,90 +23,114 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
   $locationProvider.html5Mode(true);
 
   $stateProvider.state('landing', {
-   url: '/',
-   controller: 'Landing.controller',
-   templateUrl: '/templates/landing.html'
- });
+  	url: '/',
+  	controller: 'Landing.controller',
+  	templateUrl: '/templates/landing.html'
+  });
 
   $stateProvider.state('collection', {
-   url: '/collection',
-   controller: 'Collection.controller',
-   templateUrl: '/templates/collection.html'
- });
+  	url: '/collection',
+  	controller: 'Collection.controller',
+  	templateUrl: '/templates/collection.html'
+  });
 
   $stateProvider.state('album', {
-    url: '/album',
-    controller: 'Album.controller',
-    templateUrl: '/templates/album.html'
-  })
+  	url: '/album',
+  	controller: 'Album.controller',
+  	templateUrl: '/templates/album.html'
+  });
 }]);
 
 
 blocJams.controller('Landing.controller', ['$scope', function($scope) {
-  $scope.subText= "Turn the music up!";
+	$scope.subText= "Turn the music up!";
 
-  $scope.subTextClicked = function() {
-    $scope.subText += '!';
-  };
+	$scope.subTextClicked = function() {
+		$scope.subText += '!';
+	};
 
-  $scope.albumURLs = [
-    '/images/album-placeholders/album-1.jpg',
-    '/images/album-placeholders/album-2.jpg',
-    '/images/album-placeholders/album-3.jpg',
-    '/images/album-placeholders/album-4.jpg',
-    '/images/album-placeholders/album-5.jpg',
-    '/images/album-placeholders/album-6.jpg',
-    '/images/album-placeholders/album-7.jpg',
-    '/images/album-placeholders/album-8.jpg',
-    '/images/album-placeholders/album-9.jpg',
-  ];
+	$scope.albumURLs = [
+	'/images/album-placeholders/album-1.jpg',
+	'/images/album-placeholders/album-2.jpg',
+	'/images/album-placeholders/album-3.jpg',
+	'/images/album-placeholders/album-4.jpg',
+	'/images/album-placeholders/album-5.jpg',
+	'/images/album-placeholders/album-6.jpg',
+	'/images/album-placeholders/album-7.jpg',
+	'/images/album-placeholders/album-8.jpg',
+	'/images/album-placeholders/album-9.jpg',
+	];
 
   function shuffle(o){ //v1.0
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-      return o;
+  	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  		return o;
   };
 
   $scope.shuffleAlbums = function(){
-    newOrder = shuffle($scope.albumURLs)
+  	newOrder = shuffle($scope.albumURLs)
   }
 }]);
 
 blocJams.controller('Collection.controller', ['$scope', function($scope) {
- $scope.albums = [];
- for (var i = 0; i < 33; i++) {
-   $scope.albums.push(angular.copy(albumPicasso));
- };
+	$scope.albums = [];
+	for (var i = 0; i < 33; i++) {
+		$scope.albums.push(angular.copy(albumPicasso));
+	};
 }]);
 
-blocJams.controller('Album.controller', ['$scope', function($scope) {
- $scope.album = angular.copy(albumPicasso);
+blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
+	$scope.album = angular.copy(albumPicasso);
 
- var hoveredSong = null;
- var playingSong = null;
- 
- $scope.onHoverSong = function(song) {
-   hoveredSong = song;
- };
- 
- $scope.offHoverSong = function(song) {
-   hoveredSong = null;
- };
+	var hoveredSong = null;
+	var playingSong = null;
 
- $scope.getSongState = function(song) {
-   if (song === playingSong) {
-     return 'playing';
-   }
-   else if (song === hoveredSong) {
-     return 'hovered';
-   }
-   return 'default';
- };
+	$scope.onHoverSong = function(song) {
+		hoveredSong = song;
+	};
 
-  $scope.playSong = function(song) {
-    playingSong = song;
-  };
+	$scope.offHoverSong = function(song) {
+		hoveredSong = null;
+	};
 
-  $scope.pauseSong = function(song) {
-    playingSong = null;
-  };
+	$scope.getSongState = function(song) {
+		if (song === SongPlayer.currentSong && SongPlayer.playing) {
+			return 'playing';
+		}
+		else if (song === hoveredSong) {
+			return 'hovered';
+		}
+		return 'default';
+	};
+
+	$scope.playSong = function(song) {
+		SongPlayer.setSong($scope.album, song);
+		SongPlayer.play();
+	};
+
+	$scope.pauseSong = function(song) {
+		SongPlayer.pause();
+	};
 }]);
+
+blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
+	$scope.songPlayer = SongPlayer;
+}]);
+
+blocJams.service('SongPlayer', function() {
+	return {
+		currentSong: null,
+		currentAlbum: null,
+		playing: false,
+
+		play: function() {
+			this.playing = true;
+		},
+		pause: function() {
+			this.playing = false;
+		},
+		setSong: function(album, song) {
+			this.currentAlbum = album;
+			this.currentSong = song;
+		}
+	};
+});
